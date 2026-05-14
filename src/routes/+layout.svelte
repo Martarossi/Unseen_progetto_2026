9bed34d66cde7a5e05087681010da50a72e1d340
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-05-14 18:39:02 +0200

    font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Akira Expanded -->
+	 <link href="https://db.onlinewebfonts.com/c/6fa591daa0aa2d0f845653486954aca5?family=Akira+Expanded" rel="stylesheet">
+	<!-- Helvetica -->
+	 <link rel="stylesheet" href="https://use.typekit.net/tsx2ihe.css">
+	 
 </svelte:head>
 
 {@render children()}
