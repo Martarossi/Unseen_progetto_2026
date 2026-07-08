6f85674dd09043bc885352e43817085d9cfa93f5
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-07-08 19:46:39 +0200

    tremolio

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -397,6 +397,7 @@
       font-feature-settings: "tnum" 1;
       width: 3ch;
       will-change: contents;
+      contain: layout style paint;
     }
 
     .stat-label-under {
@@ -435,6 +436,11 @@
       height: 100svh;
       min-height: unset;
       overflow: hidden;
+      /* Isola il layer sticky dal reflow del counter che cambia testo ad ogni frame:
+         senza questo, Safari iOS ricalcola la posizione sticky ad ogni mutazione,
+         facendo tremare l'intero blocco finché il numero è in animazione. */
+      transform: translateZ(0);
+      -webkit-transform: translateZ(0);
     }
 
     .stats-grid {
@@ -497,6 +503,7 @@
       font-feature-settings: "tnum" 1;
       width: 3ch;
       will-change: contents;
+      contain: layout style paint;
     }
 
     .stat-label-under {
