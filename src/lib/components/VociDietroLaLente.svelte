0f23d605d8d6d7db0e501dd5e5db5116c9d56abd
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-07-08 21:40:09 +0200

    drag to discover e tap abbassati e sistemati e abbassate anche le 2 introduzioni

--- src/lib/components/VociDietroLaLente.svelte
+++ src/lib/components/VociDietroLaLente.svelte
@@ -572,7 +572,7 @@
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, calc(-50% + 15px));
     text-align: center;
     pointer-events: none;
     white-space: nowrap;
